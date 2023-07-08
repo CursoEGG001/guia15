34e8124013a7cec714ba533adcd2386e141ff797
CursoEGG001 <soygecko@gmail.com>
2023-07-08 19:55:13 -0300

    La estancia del estanciero...

---
+++ EExtras01/src/estancias/persistencia/DAO.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estancias.persistencia;
+
+/**
+ *
+ * @author pc
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public abstract class DAO {
+
+    private Connection conexion;
+
+    public void conectar() throws SQLException {
+        if (conexion == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                String url = "jdbc:mysql://localhost:3306/estancias_exterior";
+                String usuario = "root";
+                String contraseña = "root";
+                conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/estancias_exterior", "root", "root");
+            } catch (ClassNotFoundException|SQLException e) {
+                throw new SQLException("Error al conectar con la base de datos.", e);
+            }
+        }
+    }
+
+    public void desconectar() throws SQLException {
+        if (conexion != null) {
+            try {
+                conexion.close();
+            } catch (SQLException e) {
+                throw new SQLException("Error al desconectar de la base de datos.", e);
+            }
+        }
+    }
+
+    public ResultSet consultar(String sql) throws SQLException {
+        try {
+            Statement statement = conexion.createStatement();
+            return statement.executeQuery(sql);
+        } catch (SQLException e) {
+            throw new SQLException("Error al ejecutar la consulta.", e);
+        }
+    }
+
+    public void insertar(String sql) throws SQLException {
+        try {
+            Statement statement = conexion.createStatement();
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new SQLException("Error al ejecutar la instrucción de inserción.", e);
+        }
+    }
+
+    public void modificar(String sql) throws SQLException {
+        try {
+            Statement statement = conexion.createStatement();
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new SQLException("Error al ejecutar la instrucción de modificación.", e);
+        }
+    }
+
+    public void eliminar(String sql) throws SQLException {
+        try {
+            Statement statement = conexion.createStatement();
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new SQLException("Error al ejecutar la instrucción de eliminación.", e);
+        }
+    }
+
+}
